3b8153d6c5558a735fa56465ba940bcc5afafb66
mahmoudsalah30 <mahmoud_salah30@hotmail.com>
2022-02-06 18:15:21 +0100

    hello

---
+++ fileio/inputfile2.py
@@ -0,0 +1,14 @@
+f=open('inputfile.txt','r')
+pass_file=open('pass_file.txt','w')
+fail_file=open('fail_file.txt','w')
+
+
+for  line in f  :
+     line_split=line.split()
+     if line_split[2]=='p':
+        pass_file.write(line)
+     else:
+        fail_file.write(line)
+f.close()
+pass_file.close()            
+fail_file.close()
---
+++ subprocess/script.py
@@ -0,0 +1,5 @@
+import subprocess
+
+for i in range (0,5) :
+       subprocess.check_call(['python','example.py'])
+
